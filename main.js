d29953e776485ad37f9455b19f793a5b528662fe
nodir <nodir4868@gmail.com>
2021-03-25 12:29:51 +0300

    javasc

---
+++ main.js
@@ -0,0 +1,25 @@
+
+var myName = prompt('Ismingizni kiriting');
+
+console.log('Mening ismim ' + myName);
+
+var yosh = +prompt('Yoshingizni kiriting');
+
+console.log('Men ' + yosh + ' yoshdaman');
+
+var m = prompt('Misol 1:  6 + 3 = ?');
+console.log('Misol 1: 6 + 3 = 9 Sizning javob ' + m);
+
+var m1 = prompt('Misol 2:  20 - 5 = ?');
+console.log('Misol 2: 20 - 5 = 15 Sizning javob ' + m1);
+
+var m2 = prompt('Misol 3:  15 * 2 = ?');
+console.log('Misol 3: 15 * 2 = 30 Sizning javob ' + m2);
+
+var m3 = prompt('Misol 4:  8 / 4 = ?');
+console.log('Misol 4: 8 / 4 = 2 Sizning javob ' + m3);
+
+var m4 = prompt('Misol 5:  10 % 3 = ?');
+console.log('Misol 5: 10 % 3 = 1 Sizning javob ' + m4);
+
+alert('Natijani ko`rish uchun konsolni oching')
